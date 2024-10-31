690f1b1e8bb7a91b8c24600a67ed4a3480788c9d
Szymon <Szymon@DESKTOP-KD7PK0F>
2024-10-31 15:13:37 +0100

    zej2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,33 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    Resource* obiekt_;
+
+public:
+    ResourceManager()
+    { 
+            obiekt_ = new Resource{};
+    }
+
+    ResourceManager(const ResourceManager& rm)
+    { 
+        obiekt_ = new Resource{*rm.obiekt_};
+    }
+
+    ResourceManager& operator=(const ResourceManager& rm)
+    {
+        if (this == &rm) {
+            return *this;
+        }
+        else 
+        {
+        delete obiekt_;
+        obiekt_ = new Resource{*rm.obiekt_};
+        return *this;
+        }
+    }
+
+    ~ResourceManager() { delete obiekt_;  }
+
+    double get() { return (*obiekt_).get();}
 };
